f030e732c0a2f45ec2567a1245ff8d0a455f03aa
Andrew Yin <andrewyin1@gmail.com>
2020-12-09 21:59:03 -0600

    Refactored draw method for readability

--- include/visualizer/cube_solver_app.h
+++ include/visualizer/cube_solver_app.h
@@ -4,6 +4,7 @@
 #include <core/solver.h>
 
 #include <array>
+#include <string>
 
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
@@ -73,6 +74,11 @@ class CubeSolverApp : public ci::app::App {
   std::vector<Move> solution_;
 
   /**
+   * Draws the text instructions to display on the app
+   */
+  void DrawMessages() const;
+
+  /**
    * Constructs a face of the Rubik's cube at the given location.
    *
    * @param face             The face to be drawn.
@@ -83,6 +89,29 @@ class CubeSolverApp : public ci::app::App {
   void Clear();
   /** Solves the Rubik's cube given by the user */
   void Solve();
+
+  /**
+   * Gets an instructional message to display to the app based on the current
+   * app state.
+   *
+   * @return  A string representing the instructions message
+   */
+  std::string GetInstructionsMessage() const;
+
+  /**
+   * Gets a message to display in the solution section of the app based on the
+   * current app state.
+   *
+   * @return  A string representing the solution message
+   */
+  std::string GetSolutionMessage() const;
+
+  /**
+   * Gets the corresponding Color based on the current app state of the cube
+   *
+   * @return  The color corresponding to the current state
+   */
+  Color GetCurrentStateColor() const;
 };
 
 }  // namespace visualizer
--- src/visualizer/cube_solver_app.cc
+++ src/visualizer/cube_solver_app.cc
@@ -42,71 +42,10 @@ void CubeSolverApp::setup() {
 }
 
 void CubeSolverApp::draw() {
-  ci::Color8u background_color(255, 246, 148);  // light kYellow
+  ci::Color8u background_color(255, 246, 148);  // light yellow
   ci::gl::clear(background_color);
 
-  glm::vec2 instructions_pos(kWindowWidth / 2 - 2 * kStickerWidth,
-                             kStickerWidth / 2);
-  float font_size = 36;
-
-  /* Generate top instruction message based on the app's current state */
-  std::string instructions_msg;
-  switch (current_state_) {
-    case select_red:
-      instructions_msg = "Please choose the red stickers.";
-      break;
-    case select_orange:
-      instructions_msg = "Please choose the orange stickers.";
-      break;
-    case select_blue:
-      instructions_msg = "Please choose the blue stickers.";
-      break;
-    case select_green:
-      instructions_msg = "Please choose the green stickers.";
-      break;
-    case select_yellow:
-      instructions_msg = "Please choose the yellow stickers.";
-      break;
-    case all_selected:
-      instructions_msg = "Press Enter to start solving.";
-      break;
-    case solved:
-      instructions_msg =
-          "Please view the solution on the right (if one exists).";
-      break;
-  }
-
-  /* Generate a message for where the solution is to be placed based on the
-   * app state */
-  std::string solution_msg;
-  if (current_state_ == solved) {
-    std::string separator = "";
-    for (const Move& move : solution_) {
-      solution_msg += separator + MoveToString(move);
-      separator = " ";
-    }
-  } else {
-    solution_msg = "N/A";
-  }
-
-  ci::gl::drawStringCentered(instructions_msg, instructions_pos,
-                             ci::Color("black"), ci::Font("Arial", font_size));
-
-  ci::gl::drawStringCentered("Solution",
-                             glm::vec2(kWindowWidth / 2 + 5 * kStickerWidth,
-                                       kWindowHeight / 2 - kStickerWidth),
-                             ci::Color("black"), ci::Font("Arial", font_size));
-
-  ci::gl::drawStringCentered(
-      "Press Backspace to clear the whole cube or click once more to reset an "
-      "individual sticker.",
-      glm::vec2(kWindowWidth / 2 - 2 * kStickerWidth,
-                kWindowHeight - kStickerWidth),
-      ci::Color("black"), ci::Font("Arial", 20));
-  ci::gl::drawStringCentered(
-      solution_msg,
-      glm::vec2(kWindowWidth / 2 + 5 * kStickerWidth, kWindowHeight / 2),
-      ci::Color("black"), ci::Font("Arial", 24));
+  DrawMessages();
 
   /* Draw each individual sticker of the cube */
   for (size_t i = 0; i < kNumFaces; i++) {
@@ -123,26 +62,7 @@ void CubeSolverApp::mouseDown(ci::app::MouseEvent event) {
       for (size_t j = 0; j < kNumCornersPerFace; j++) {
         Sticker& sticker = stickers_[i][j];
         if (sticker.IsWithinSticker(mouse_pos)) {
-          Color color;
-          switch (current_state_) {
-            case select_red:
-              color = kRed;
-              break;
-            case select_orange:
-              color = kOrange;
-              break;
-            case select_blue:
-              color = kBlue;
-              break;
-            case select_green:
-              color = kGreen;
-              break;
-            case select_yellow:
-              color = kYellow;
-              break;
-            default:
-              break;
-          }
+          Color color = GetCurrentStateColor();
 
           Color sticker_color = sticker.GetColor();
           if (sticker_color == kWhite) {
@@ -152,7 +72,6 @@ void CubeSolverApp::mouseDown(ci::app::MouseEvent event) {
             sticker.SetColor(kWhite);
             color_count_--;
           }
-
           if (color_count_ == kNumCornersPerFace) {
             color_count_ = 0;
             ++current_state_;
@@ -175,6 +94,33 @@ void CubeSolverApp::keyDown(ci::app::KeyEvent event) {
       Clear();
       break;
   }
+}
+
+void CubeSolverApp::DrawMessages() const {
+  float font_size = 36;
+  glm::vec2 instructions_pos(kWindowWidth / 2 - 2 * kStickerWidth,
+                             kStickerWidth / 2);
+  std::string instructions_msg = GetInstructionsMessage();
+  std::string solution_msg = GetSolutionMessage();
+
+  ci::gl::drawStringCentered(instructions_msg, instructions_pos,
+                             ci::Color("black"), ci::Font("Arial", font_size));
+  ci::gl::drawStringCentered(
+      solution_msg,
+      glm::vec2(kWindowWidth / 2 + 5 * kStickerWidth, kWindowHeight / 2),
+      ci::Color("black"), ci::Font("Arial", 24));
+
+  ci::gl::drawStringCentered("Solution",
+                             glm::vec2(kWindowWidth / 2 + 5 * kStickerWidth,
+                                       kWindowHeight / 2 - kStickerWidth),
+                             ci::Color("black"), ci::Font("Arial", font_size));
+
+  ci::gl::drawStringCentered(
+      "Press Backspace to clear the whole cube or click once more to reset an "
+      "individual sticker.",
+      glm::vec2(kWindowWidth / 2 - 2 * kStickerWidth,
+                kWindowHeight - kStickerWidth),
+      ci::Color("black"), ci::Font("Arial", 20));
 }
 
 void CubeSolverApp::CreateFace(const Face& face,
@@ -206,6 +152,66 @@ void CubeSolverApp::Solve() {
     }
   }
   solution_ = solver_.SolveCube(Cube(colors));
+}
+
+std::string CubeSolverApp::GetInstructionsMessage() const {
+  std::string instructions_msg;
+  switch (current_state_) {
+    case select_red:
+      instructions_msg = "Please choose the red stickers.";
+      break;
+    case select_orange:
+      instructions_msg = "Please choose the orange stickers.";
+      break;
+    case select_blue:
+      instructions_msg = "Please choose the blue stickers.";
+      break;
+    case select_green:
+      instructions_msg = "Please choose the green stickers.";
+      break;
+    case select_yellow:
+      instructions_msg = "Please choose the yellow stickers.";
+      break;
+    case all_selected:
+      instructions_msg = "Press Enter to start solving.";
+      break;
+    case solved:
+      instructions_msg =
+          "Please view the solution on the right (if one exists).";
+      break;
+  }
+  return instructions_msg;
+}
+
+std::string CubeSolverApp::GetSolutionMessage() const {
+  std::string solution_msg;
+  if (current_state_ == solved) {
+    std::string separator = "";
+    for (const Move& move : solution_) {
+      solution_msg += separator + MoveToString(move);
+      separator = " ";
+    }
+  } else {
+    solution_msg = "N/A";
+  }
+  return solution_msg;
+}
+
+Color CubeSolverApp::GetCurrentStateColor() const {
+  switch (current_state_) {
+    case select_red:
+      return kRed;
+    case select_orange:
+      return kOrange;
+    case select_blue:
+      return kBlue;
+    case select_green:
+      return kGreen;
+    case select_yellow:
+      return kYellow;
+    default:
+      return kWhite;
+  }
 }
 
 }  // namespace visualizer
